7ec6f5859e584f2b67b49504d8c62f5c5e35f141
manoel <candidomanoel78@gmail.com>
2025-02-05 16:38:59 -0300

    completing the event cycle

--- webhooks/views.py
+++ webhooks/views.py
@@ -5,6 +5,9 @@ class WebhookOrderView(views.APIView):
     
     def post(self, request):
         data = request.data
+
+        print(data)
+
         return response.Response(
             data=data,
             status=status.HTTP_200_OK,
