7441792015218763f710edb69a4ee02fa9878214
manoel <candidomanoel78@gmail.com>
2025-02-06 15:01:49 -0300

    building and sending the message

---
+++ webhooks/messages.py
@@ -0,0 +1,8 @@
+outflow_message = '''
+*Olá, uma nova saída foi registrada!*
+
+Produto: *{}*
+Quantidade: *{}*
+Valor da venda: *R$ {}*
+Lucro com a venda: *R$ {}*
+'''
--- webhooks/views.py
+++ webhooks/views.py
@@ -1,6 +1,8 @@
 import json
 from rest_framework import views, response, status
 from webhooks import models
+from webhooks.messages import outflow_message
+from services.callmebot import CallMeBot
 
 
 class WebhookOrderView(views.APIView):
@@ -12,6 +14,23 @@ class WebhookOrderView(views.APIView):
             event_type=data.get('event_type'),
             event=json.dumps(data, ensure_ascii=False),
         )
+
+        product_title = data.get('product')
+        quantity = data.get('quantity')
+        product_cost_price = data.get('product_cost_price')
+        product_selling_price = data.get('product_selling_price')
+        total_value = product_selling_price * quantity
+        profit_value = total_value - (product_cost_price * quantity)
+
+        message = outflow_message.format(
+            product_title, 
+            quantity,
+            total_value,
+            profit_value,
+        )
+
+        callmebot = CallMeBot()
+        callmebot.send_message(message)
 
         return response.Response(
             data=data,
